6f3e2d9433a9ad0688ff9e51c3a443f6f871f47b
gerry <krittameth2020@gmail.com>
2026-03-06 10:32:42 +0700

    fix typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,9 @@ VITE_GRAPHQL_URI="${VITE_GRAPHQL_URI:-http://54.90.216.90:8082/graphql}"
 VITE_SERVER_URI="${VITE_SERVER_URI:-http://54.90.216.90:8082}"
 
 find /usr/share/nginx/html/assets -name '*.js' -exec sed -i \
-    "s|_VITE_GRAPHQL_URI_PLACEHOLER_|${VITE_GRAPHQL_URI}|g" {} +
+    "s|_VITE_GRAPHQL_URI_PLACEHOLDER_|${VITE_GRAPHQL_URI}|g" {} +
 find /usr/share/nginx/html/assets -name '*.js' -exec sed -i \
-    "s|_VITE_SERVER_URI_PLACEHOLER_|${VITE_SERVER_URI}|g" {} +
+    "s|_VITE_SERVER_URI_PLACEHOLDER_|${VITE_SERVER_URI}|g" {} +
 
 echo "Configured VITE_GRAPHQL_URI=${VITE_GRAPHQL_URI}"
 echo "Configured VITE_SERVER_URI=${VITE_SERVER_URI}"
